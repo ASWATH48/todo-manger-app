da3c416b367869c60eb25c64759fb48d665ea22e
ASWATH48 <kingaswath46@gmail.com>
2022-06-29 12:01:46 +0530

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  
+ root 'main#index'
   # get 'main/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   match 'todo',:to => 'main#index',:via => :get
 
- root 'main#index'
  match 'main/create',:to => 'main#create',:via => :post
- match '/delete/:id' ,:to =>  'main#delete',:via => :get
+#  match '/delete/:id' ,:to =>  'main#delete',:via => :get
 
-#  get '/delete/:id' => 'main#delete',as: :delete
+ get '/delete/:id' => 'main#delete',as: :delete
 
 
 end
